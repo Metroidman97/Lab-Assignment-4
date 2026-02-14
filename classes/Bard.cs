886f78bdf3a62aa3c6a1df175cd6a596a9f2420a
Metroidman97 <78391681+Metroidman97@users.noreply.github.com>
2026-02-13 21:14:46 -0500

    Add files via upload

--- classes/Bard.cs
+++ classes/Bard.cs
@@ -6,4 +6,32 @@ public class Bard : Character
 {
     public int dieType = 8;     // d8
     public int dieAverage = 5;  // real average 4.5
+
+    public Bard(string name, int level, string characterClass, int conScore, string race, bool hasTough, bool hasStout, bool hPAveraged) : base(name, level, characterClass, conScore, race, hasTough, hasStout, hPAveraged)
+    {
+
+    }
+    public void CalculateHP()
+    {
+        //GetCharacterClass();
+        //GetCharacterRace();
+
+        for (int i = 1; i <= level; i++)
+        {
+            int tempHP;
+
+            if (hPAveraged)
+            {
+                tempHP = dieAverage + GetConModifier() + GetRaceBonus() + GetToughBonus() + GetStoutBonus();
+                finalHP += tempHP;
+            }
+            else if (!hPAveraged)
+            {
+                tempHP = Random.Range(1, dieType + 1) + GetConModifier() + GetRaceBonus() + GetToughBonus() + GetStoutBonus();
+                finalHP += tempHP;
+            }
+        }
+
+        Debug.Log("Name: " + name + ", Final HP: " + finalHP);
+    }
 }
--- classes/Ranger.cs
+++ classes/Ranger.cs
@@ -6,4 +6,32 @@ public class Ranger : Character
 {
     public int dieType = 10;     // d10
     public int dieAverage = 6;   // real average 5.5
+
+    public Ranger(string name, int level, string characterClass, int conScore, string race, bool hasTough, bool hasStout, bool hPAveraged) : base(name, level, characterClass, conScore, race, hasTough, hasStout, hPAveraged)
+    {
+
+    }
+    public void CalculateHP()
+    {
+        //GetCharacterClass();
+        //GetCharacterRace();
+
+        for (int i = 1; i <= level; i++)
+        {
+            int tempHP;
+
+            if (hPAveraged)
+            {
+                tempHP = dieAverage + GetConModifier() + GetRaceBonus() + GetToughBonus() + GetStoutBonus();
+                finalHP += tempHP;
+            }
+            else if (!hPAveraged)
+            {
+                tempHP = Random.Range(1, dieType + 1) + GetConModifier() + GetRaceBonus() + GetToughBonus() + GetStoutBonus();
+                finalHP += tempHP;
+            }
+        }
+
+        Debug.Log("Name: " + name + ", Final HP: " + finalHP);
+    }
 }
